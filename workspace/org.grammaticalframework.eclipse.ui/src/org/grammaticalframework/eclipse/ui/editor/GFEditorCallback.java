babe032d7c50e682354bd6d79c9a51caffb1c6b7
John J. Camilleri <john@Prinjolata.(none)>
2011-11-29 09:49:45 +0100

    New release.

--- workspace/org.grammaticalframework.eclipse.ui/src/org/grammaticalframework/eclipse/ui/editor/GFEditorCallback.java
+++ workspace/org.grammaticalframework.eclipse.ui/src/org/grammaticalframework/eclipse/ui/editor/GFEditorCallback.java
@@ -1,9 +1,19 @@
+/**
+ * GF Eclipse Plugin
+ * http://www.grammaticalframework.org/eclipse/
+ * John J. Camilleri, 2011
+ * 
+ * The research leading to these results has received funding from the
+ * European Union's Seventh Framework Programme (FP7/2007-2013) under
+ * grant agreement n° FP7-ICT-247914.
+ */
 package org.grammaticalframework.eclipse.ui.editor;
 
 import org.eclipse.xtext.ui.editor.AbstractDirtyStateAwareEditorCallback;
 
 /**
  * The only point of this is to prevent the "Add Xtext nature" prompt
+ * 
  * @author John J. Camilleri
  *
  */
