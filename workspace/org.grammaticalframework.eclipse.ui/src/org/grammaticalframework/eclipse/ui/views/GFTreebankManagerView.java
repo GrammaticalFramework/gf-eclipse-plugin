71395daeb50bef386b7b24bb48b53ada2f6d7c57
John J. Camilleri <john@Prinjolata.(none)>
2012-02-23 16:01:12 +0100

    Added new treebank view, changed library browser icons

---
+++ workspace/org.grammaticalframework.eclipse.ui/src/org/grammaticalframework/eclipse/ui/views/GFTreebankManagerView.java
@@ -0,0 +1,24 @@
+package org.grammaticalframework.eclipse.ui.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class GFTreebankManagerView extends ViewPart {
+
+	public GFTreebankManagerView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
