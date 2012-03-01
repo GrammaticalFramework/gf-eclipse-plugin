7d8aaa96a019c1c41867fb1d4945fafef2767baf
John J. Camilleri <john@Prinjolata.(none)>
2012-03-01 11:57:06 +0100

    Check viewer selection when action is run

--- workspace/org.grammaticalframework.eclipse.ui/src/org/grammaticalframework/eclipse/ui/views/RunTreebankAction.java
+++ workspace/org.grammaticalframework.eclipse.ui/src/org/grammaticalframework/eclipse/ui/views/RunTreebankAction.java
@@ -10,6 +10,7 @@ import org.eclipse.debug.core.ILaunchManager;
 import org.eclipse.debug.ui.DebugUITools;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.widgets.Event;
@@ -34,24 +35,37 @@ public class RunTreebankAction extends Action {
 
 	public void run() {
 		
-		// See what's selected in our viewer
-		ISelection selection = viewer.getSelection();
-		// TODO
+		// See what's selected in our viewer and validate
+		IStructuredSelection selection = (IStructuredSelection)viewer.getSelection();
+		if (selection.isEmpty()) {
+			return;
+		}
+		try {
+			treebankFile = (IFile)selection.getFirstElement();
+		} catch (ClassCastException _) {
+			log.warn("Can only run action on file.");
+			return;
+		}
+		goldStandardFile = GFTreebankManagerHelper.getGoldStandardFile(treebankFile);
+		if (goldStandardFile == null) {
+			log.warn("Found no corresponding gold standard for "+treebankFile.getName());
+			return;
+		}
 		
 		ILaunchConfigurationType gfLaunchType = DebugPlugin.getDefault().getLaunchManager().getLaunchConfigurationType("org.grammaticalframework.eclipse.GFLaunchConfigurationType");
 		ILaunchConfigurationWorkingCopy config;
 		try {
 			config = gfLaunchType.newInstance(null, "On-the-fly GF Treebank Lanuch");
 			config.setAttribute(IGFLaunchConfigConstants.WORKING_DIR, treebankFile.getParent().getLocation().toOSString());
-			config.setAttribute(IGFLaunchConfigConstants.FILENAMES, "*");
-//			config.setAttribute(IGFLaunchConfigConstants.OPTIONS, "");
+			config.setAttribute(IGFLaunchConfigConstants.FILENAMES, "");
+			config.setAttribute(IGFLaunchConfigConstants.OPTIONS, "");
 			config.setAttribute(IGFLaunchConfigConstants.INTERACTIVE_MODE, false);
 			config.setAttribute(IGFLaunchConfigConstants.BATCH_MODE, true);
 			config.setAttribute(IGFLaunchConfigConstants.TREEBANK_MODE, true);
 			config.setAttribute(IGFLaunchConfigConstants.TREEBANK_FILENAME, treebankFile.getName());
 			config.setAttribute(IGFLaunchConfigConstants.GOLD_STANDARD_FILENAME, goldStandardFile.getName());
 
-			DebugUITools.launch(config, ILaunchManager.RUN_MODE);		
+			DebugUITools.launch(config, ILaunchManager.RUN_MODE);
 			
 		} catch (CoreException e) {
 			log.error("Couldn't run treebank action", e);
