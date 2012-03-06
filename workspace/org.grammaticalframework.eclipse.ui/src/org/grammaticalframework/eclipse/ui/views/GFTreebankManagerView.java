ae499e1759b423af16fe9aeff64624952b8670b2
John J. Camilleri <john@Prinjolata.(none)>
2012-03-06 11:25:20 +0100

    Catch a NPE

--- workspace/org.grammaticalframework.eclipse.ui/src/org/grammaticalframework/eclipse/ui/views/GFTreebankManagerView.java
+++ workspace/org.grammaticalframework.eclipse.ui/src/org/grammaticalframework/eclipse/ui/views/GFTreebankManagerView.java
@@ -346,6 +346,8 @@ public class GFTreebankManagerView extends ViewPart {
 							return true; // keep looking
 						}
 					});
+				} catch (NullPointerException e) {
+					// 
 				} catch (CoreException e) {
 					// 
 				}
