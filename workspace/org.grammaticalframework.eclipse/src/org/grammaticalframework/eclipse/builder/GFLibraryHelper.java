9e8bc7b807601aada05a5c8d1cc8a383f137a566
John J. Camilleri <john@Prinjolata.(none)>
2012-03-12 15:53:21 +0100

    removed a bunch of unused methods

--- workspace/org.grammaticalframework.eclipse/src/org/grammaticalframework/eclipse/builder/GFLibraryHelper.java
+++ workspace/org.grammaticalframework.eclipse/src/org/grammaticalframework/eclipse/builder/GFLibraryHelper.java
@@ -12,7 +12,6 @@ package org.grammaticalframework.eclipse.builder;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.xtext.EcoreUtil2;
 
 /**
  * For resolving GF library modules and their exported definitions.
@@ -22,92 +21,18 @@ import org.eclipse.xtext.EcoreUtil2;
 public class GFLibraryHelper {
 	
 	/**
-	 * Put together path to local gf source file.
-	 *
-	 * @param context the context
-	 * @param moduleName the module name
-	 * @return the local source path
-	 */
-	private static String getLocalSourcePath(Resource context, String moduleName) {
-		URI uri = context.getURI();
-		return uri.trimSegments(1) + java.io.File.separator + moduleName + ".gf";
-	}
-	
-	/**
 	 * Put together path to compiled tags file in the build folder.
 	 *
 	 * @param context the context
-	 * @param moduleName the module name
 	 * @return the header path
 	 */
-	public static URI getTagsFile(Resource context, String moduleName) {
-		String sb = GFBuilderHelper.getTagsFileNameRelative(context.getURI().lastSegment());
-		URI uri = URI.createURI(sb);
+	public static URI getTagsFile(Resource context) {
+		URI uri = GFBuilderHelper.getTagsFileURIRelative(context.getURI().lastSegment());
 		return uri.resolve(context.getURI());
 	}
 	
 	/**
-	 * Put together path to compiled gfh header in the build folder.
-	 *
-	 * @param context the context
-	 * @param moduleName the module name
-	 * @return the header path
-	 */
-	private static String getHeaderPath(Resource context, String moduleName) {
-		StringBuilder sb = new StringBuilder();
-		
-		// Are we "outside" the gfbuild folder?
-		if (!context.getURI().trimSegments(1).lastSegment().equals(GFBuilder.BUILD_FOLDER)) {
-			sb.append(GFBuilder.BUILD_FOLDER + java.io.File.separator);
-		}
-		sb.append(moduleName + ".gfh");
-		return sb.toString();
-	}
-	
-	/**
-	 * Check if a module exists.
-	 *
-	 * @param context the context
-	 * @param moduleName the module name
-	 * @return the module uri
-	 */
-	public static URI getModuleURI(Resource context, String moduleName) {
-		URI uri;
-		
-		// First try a local file
-		uri = URI.createURI( getLocalSourcePath(context, moduleName) );
-		if (!EcoreUtil2.isValidUri(context, uri)) {
-			// Else try a compiled file
-			uri = URI.createURI( getHeaderPath(context, moduleName) );
-		}
-		
-		// Return as absolute URL
-		if (EcoreUtil2.isValidUri(context, uri)) {
-			return uri.resolve(context.getURI());
-		} else {
-			return null;
-		}
-	}
-	
-	
-	/**
-	 * Gets the module resource.
-	 *
-	 * @param context the context
-	 * @param moduleName the module name
-	 * @return the module resource
-	 */
-	public static Resource getModuleResource(Resource context, String moduleName) {
-		URI uri = getModuleURI(context, moduleName);
-		try {
-			return EcoreUtil2.getResource(context, uri.toString() );
-		} catch (NullPointerException e) {
-			return null;
-		}
-	}
-	
-	/**
-	 * 
+	 * Is the given resource linked?
 	 * @param resource
 	 * @return
 	 */
@@ -122,17 +47,5 @@ public class GFLibraryHelper {
 		return uri.segment(uri.segmentCount()-2).equals(GFBuilder.EXTERNAL_FOLDER);
 	}
 
-	
-//	/**
-//	 * Check if a module exists.
-//	 *
-//	 * @param context the context
-//	 * @param moduleName the module name
-//	 * @return true, if successful
-//	 */
-//	public boolean moduleExists(Resource context, String moduleName) {
-//		return (getModuleURI(context, moduleName) != null);
-//	}
-	
 
 }
