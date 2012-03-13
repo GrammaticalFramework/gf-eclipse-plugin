fa8a2ef152ad023f9943aa83e1ca232600a9257d
John J. Camilleri <john@Prinjolata.(none)>
2012-03-13 14:12:14 +0100

    Library view now respects new EXTERNAL_FOLDERs

--- workspace/org.grammaticalframework.eclipse.ui/src/org/grammaticalframework/eclipse/ui/views/GFLibraryTreeView.java
+++ workspace/org.grammaticalframework.eclipse.ui/src/org/grammaticalframework/eclipse/ui/views/GFLibraryTreeView.java
@@ -9,7 +9,6 @@
  */
 package org.grammaticalframework.eclipse.ui.views;
 
-
 import org.apache.log4j.Logger;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
@@ -22,27 +21,16 @@ import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.SWT;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
-import org.eclipse.core.resources.IProject;
-import org.grammaticalframework.eclipse.builder.GFBuilder;
+import org.grammaticalframework.eclipse.scoping.GFScopingHelper;
 import org.grammaticalframework.eclipse.ui.labeling.GFImages;
 
 import com.google.inject.Inject;
 
 /**
- * This sample class demonstrates how to plug-in a new workbench view. The view
- * shows data obtained from the model. The sample creates a dummy model on the
- * fly, but a real implementation would connect to the model available either in
- * this or another plug-in (e.g. the workspace). The view is connected to the
- * model using a content provider.
- * <p>
- * The view uses a label provider to define how model objects should be
- * presented in the view. Each view can present the same model objects using
- * different labels and icons, if needed. Alternatively, a single label provider
- * can be shared between views in order to ensure that objects of the same type
- * are presented in the same way everywhere.
- * <p>
+ * View for browsing all external modules references by the active file.
+ * @author John J. Camilleri
+ *
  */
-
 public class GFLibraryTreeView extends ViewPart {
 
 	/**
@@ -109,9 +97,8 @@ public class GFLibraryTreeView extends ViewPart {
 					IEditorInput input = editor.getEditorInput();
 					if (input instanceof IFileEditorInput) {
 						IFile file = ((IFileEditorInput) input).getFile();
-						IProject project = file.getProject();
-						IFolder buildFolder = project.getFolder(GFBuilder.EXTERNAL_FOLDER);
-						viewer.setInput(buildFolder);
+						IFolder extFolder = GFScopingHelper.getExernalFolder(file);
+						viewer.setInput(extFolder);
 					}
 				} catch (Exception _) {
 					viewer.setInput(null);
--- workspace/org.grammaticalframework.eclipse/src/org/grammaticalframework/eclipse/scoping/GFScopingHelper.java
+++ workspace/org.grammaticalframework.eclipse/src/org/grammaticalframework/eclipse/scoping/GFScopingHelper.java
@@ -1,6 +1,7 @@
 package org.grammaticalframework.eclipse.scoping;
 
 import org.apache.log4j.Logger;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
@@ -71,23 +72,37 @@ public class GFScopingHelper {
 	}
 	
 	/**
+	 * Get the external folder for a given file. Does <strong>not</strong> create the folder if
+	 * it doesn't exist.
+	 * @param file
+	 * @return The external folder, or possibly <code>null</code> if it doesn't exist
+	 */
+	public static IFolder getExernalFolder(IFile file) {
+		if (GFBuilder.USE_GLOBAL_EXTERNAL_FOLDER) {
+			return file.getProject().getFolder(GFBuilder.EXTERNAL_FOLDER);
+		} else {
+			return file.getParent().getFolder(new Path(GFBuilder.EXTERNAL_FOLDER));
+		}
+	}
+	
+	/**
 	 * The the external folder for a tag file URI. Expected types of URI are:
 	 * <ul>
 	 * 		<li><code>platform:/resource/Foods/.gfbuild/FoodsEng.gf-tags</code></li>
 	 * 		<li><code>file:/home/john/repositories/gf-eclipse-plugin/workspace-demo/Foods/.gfbuild/FoodsEng.gf-tags</code></li>
 	 * </ul>
-	 * @param uri
+	 * @param tagFileURI
 	 * @return
 	 * @throws CoreException
 	 */
-	public static IFolder getExternalFolder(URI uri) throws CoreException {
+	public static IFolder getExternalFolder(URI tagFileURI) throws CoreException {
 		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
 		IResource res = null;
-		if (uri.isPlatform()) {
-			res = root.findMember(uri.toPlatformString(true));
-		} else if (uri.isFile()) {
+		if (tagFileURI.isPlatform()) {
+			res = root.findMember(tagFileURI.toPlatformString(true));
+		} else if (tagFileURI.isFile()) {
 			String workspaceStem = ResourcesPlugin.getWorkspace().getRoot().getRawLocationURI().toString() + java.io.File.separator;
-			URI projectURI = uri.deresolve(URI.createURI(workspaceStem));
+			URI projectURI = tagFileURI.deresolve(URI.createURI(workspaceStem));
 			res = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(projectURI.toFileString()));
 		} else {
 			log.error("Unexpected URI format");
